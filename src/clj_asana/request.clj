c576890d1f0ab5744db9dcddd121c8924764a868
Akash Kothawale <akash@decached.com>
2015-03-07 03:08:13 +0530

    Common function to hit Asana with a PUT request

--- src/clj_asana/request.clj
+++ src/clj_asana/request.clj
@@ -13,6 +13,7 @@
   Args:
   api-key: Api key of a user
   endpoint: URI path for request
+  params: (Optional) query parameters for the request
   "
   [api-key endpoint & {:keys [params]}]
   (let [response (client/get (format "%s/%s" api-url endpoint)
@@ -20,6 +21,26 @@
                               :query-params params
                               :as :json
                               :content-type :json
+                              :throw-entire-message? true})]
+    (if (= 200 (:status response))
+      (:data (:body response)))))
+
+(defn api-put
+  "Peforms a PUT request
+
+  Args:
+  api-key: Api key of a user
+  endpoint: URI path for request
+  data: Payload for the request
+  params: (Optional) query parameters for the request
+  "
+  [api-key endpoint data & {:keys [params]}]
+  (let [response (client/put (format "%s/%s" api-url endpoint)
+                             {:basic-auth [api-key ""]
+                              :query-params params
+                              :as :json
+                              :content-type :json
+                              :form-params {"data" data}
                               :throw-entire-message? true})]
     (if (= 200 (:status response))
       (:data (:body response)))))
