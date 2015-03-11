16a92e854063120f5eb5977c6d2cd8056e2539ee
Akash Kothawale <akash@decached.com>
2015-03-12 00:08:37 +0530

    Create and update tags

--- src/clj_asana/tags.clj
+++ src/clj_asana/tags.clj
@@ -1,6 +1,22 @@
 (ns clj-asana.tags
   (:require [clj-asana.request :as request]))
 
+(defn create-tag
+  "Creates a tag in the given workspace
+
+  Args:
+  workspace-id: id# of a workspace
+  tag-name: Name for the tag
+  color: (Optional) Color for the tag
+  notes: (Optional) Notes for the tag
+  "
+  [api-key workspace-id tag-name & {:keys [color notes] :or {:color nil :notes nil}}]
+  (request/api-post api-key
+                    (format "workspaces/%s/tags" workspace-id)
+                    (conj {:name tag-name}
+                          (if color {:color color})
+                          (if notes {:notes notes}))))
+
 (defn show-tag
   "Shows info about a tag
 
@@ -9,6 +25,23 @@
   "
   [api-key tag-id]
   (request/api-get api-key (format "tags/%s" tag-id)))
+
+(defn update-tag
+  "Creates a tag in the given workspace
+
+  Args:
+  tag-id: id# of tag
+  tag-name: (Optional) Name for the tag
+  color: (Optional) Color for the tag
+  notes: (Optional) Notes for the tag
+  "
+  [api-key tag-id & {:keys [tag-name color notes] :or {:tag-name nil :color nil :notes nil}}]
+  (request/api-put api-key
+                    (format "tags/%s" tag-id)
+                    (conj {}
+                          (if tag-name {:name tag-name})
+                          (if color {:color color})
+                          (if notes {:notes notes}))))
 
 (defn list-tagged-tasks
   "Gets tasks for a tag
